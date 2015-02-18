f1a8019f6b3f2d49d79aedd06b0c32190f582414
Patrick San Juan <pat.sanjuan@gmail.com>
2015-02-18 11:49:04 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	// Tooltips for bottom-right utilities menu
+	$('.six-resources-tools').tooltip({
+		position: {
+			my: "bottom-60",
+			of: "body"
+		}
+	});
+
+});
